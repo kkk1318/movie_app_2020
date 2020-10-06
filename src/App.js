fd890bcf773d2243a647377814093e291e06aeda
kkk1318 <kkk1318@hanmail.net>
2020-10-06 20:45:48 +0900

    05 스테이트와 클래스 컴퍼넌트

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component {
+  state ={
+    isLoading: true,
+    movies: [],
+  };
+
+  componentDidMount() {
+    // 영화데이터 로딩
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 6000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+    <div>
+      {isLoading ? 'loading...' : 'We are ready'}
+    </div>
+
+    );
+  }
+}
+
+export default App;
