4a9b778cc86914761fa350def310f0543502cc72
kkk1318 <kkk1318@hanmail.net>
2020-10-10 15:45:02 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
+import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-  state ={
-    isLoading: true,
-    movies: [],
-  };
 
-  componentDidMount() {
-    // 영화데이터 로딩
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
-  }
-
-  render() {
-    const { isLoading } = this.state;
-    return (
-    <div>
-      {isLoading ? 'loading...' : 'We are ready'}
-    </div>
-
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "니꼴라스 쌤의 리액트강좌 : 영화 평점사이트 만들기"
+            </span>
+        </div>
+    );
+}
+
+export default About;
